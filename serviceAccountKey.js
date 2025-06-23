b7dce35d02bea1735dcb726c15226e110bbaf51c
rashad262626 <rashadpps6@gmail.com>
2025-06-23 11:53:02 -0700

    Update serviceAccountKey.js

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -1,10 +1,9 @@
-const admin = require('firebase-admin');
-
+require('dotenv').config();
 const serviceAccount = {
   type: process.env.GOOGLE_TYPE,
-  project_id: 'aborgiba', // مباشرة نص ثابت للتجربة
+  project_id: 'aborgiba', // or process.env.GOOGLE_PROJECT_ID
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // must replace literal "\n"
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: process.env.GOOGLE_AUTH_URI,
@@ -14,15 +13,4 @@ const serviceAccount = {
   universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
 };
 
-console.log('Firebase Service Account:', serviceAccount);
-console.log('Type of project_id:', typeof serviceAccount.project_id);
-console.log('serviceAccount keys:', Object.keys(serviceAccount));
-console.log('project_id typeof:', typeof serviceAccount.project_id);
-console.log('project_id value:', serviceAccount.project_id);
-console.log('isPlainObject:', Object.getPrototypeOf(serviceAccount) === Object.prototype);
-console.log('serviceAccount JSON:', JSON.stringify(serviceAccount));
-
-const serviceAccount = require('./serviceAccountKey');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+module.exports = serviceAccount;
