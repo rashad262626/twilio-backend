6909090885ec003cdba5b08971a9be772ce4c8e7
rashad262626 <rashadpps6@gmail.com>
2025-06-23 10:47:28 -0700

    Update serviceAccountKey.js

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -2,19 +2,18 @@
 
 // Firebase Admin Setup
 const admin = require('firebase-admin');
-
 const serviceAccount = {
-  type: process.env.GOOGLE_TYPE,
-  project_id: process.env.GOOGLE_PROJECT_ID,
-  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  client_id: process.env.GOOGLE_CLIENT_ID,
-  auth_uri: process.env.GOOGLE_AUTH_URI,
-  token_uri: process.env.GOOGLE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+  type: String(process.env.GOOGLE_TYPE),
+  project_id: String(process.env.GOOGLE_PROJECT_ID),
+  private_key_id: String(process.env.GOOGLE_PRIVATE_KEY_ID),
+  private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(/\\n/g, '\n'),
+  client_email: String(process.env.GOOGLE_CLIENT_EMAIL),
+  client_id: String(process.env.GOOGLE_CLIENT_ID),
+  auth_uri: String(process.env.GOOGLE_AUTH_URI),
+  token_uri: String(process.env.GOOGLE_TOKEN_URI),
+  auth_provider_x509_cert_url: String(process.env.GOOGLE_AUTH_PROVIDER_CERT_URL),
+  client_x509_cert_url: String(process.env.GOOGLE_CLIENT_CERT_URL),
+  universe_domain: String(process.env.GOOGLE_UNIVERSE_DOMAIN),
 };
 console.log('Firebase Service Account:', serviceAccount);
 
