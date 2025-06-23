3a0d5916f1cb563f2bfa527a688cdbbd47dbf8e9
rashad262626 <rashadpps6@gmail.com>
2025-06-23 11:05:08 -0700

    Update serviceAccountKey.js

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -1,23 +1,22 @@
 const admin = require('firebase-admin');
-require('dotenv').config(); // if using local .env
 
 const serviceAccount = {
-  type: String(process.env.GOOGLE_TYPE || 'service_account'),
-  project_id: String(process.env.GOOGLE_PROJECT_ID),
-  private_key_id: String(process.env.GOOGLE_PRIVATE_KEY_ID),
-  private_key: String(process.env.GOOGLE_PRIVATE_KEY).replace(/\\n/g, '\n'),
-  client_email: String(process.env.GOOGLE_CLIENT_EMAIL),
-  client_id: String(process.env.GOOGLE_CLIENT_ID),
-  auth_uri: String(process.env.GOOGLE_AUTH_URI),
-  token_uri: String(process.env.GOOGLE_TOKEN_URI),
-  auth_provider_x509_cert_url: String(process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL),
-  client_x509_cert_url: String(process.env.GOOGLE_CLIENT_X509_CERT_URL),
-  universe_domain: String(process.env.GOOGLE_UNIVERSE_DOMAIN || 'googleapis.com')
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // must replace literal "\n"
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
 };
 
 console.log('Firebase Service Account:', serviceAccount);
 console.log('Type of project_id:', typeof serviceAccount.project_id);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
