781c16cb351d0e37647f546f457d0a0d42ae9e6c
rashad262626 <rashadpps6@gmail.com>
2025-06-23 10:21:10 -0700

    Update serviceAccountKey.js

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
 
+// Firebase Admin Setup
+const admin = require('firebase-admin');
+
 const serviceAccount = {
-  type: process.env.GOOGLE_TYPE || 'service_account',
+  type: process.env.GOOGLE_TYPE,
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: process.env.GOOGLE_AUTH_URI,
   token_uri: process.env.GOOGLE_TOKEN_URI,
   auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
   client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
 };
 
-module.exports = serviceAccount;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Twilio Setup
+const twilioClient = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
