5043c913aa92d87028f640d63275d3882bf75290
rashad262626 <rashadpps6@gmail.com>
2025-06-23 10:42:05 -0700

    Update serviceAccountKey.js

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -16,6 +16,7 @@ const serviceAccount = {
   client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
   universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
 };
+console.log('Firebase Service Account:', serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
